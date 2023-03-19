27915f189102adde9d2265cb2bda1d0594101536
anusha <anusha.gatti99@gmail.com>
2023-03-19 15:08:11 +0000

    reset test1

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello world changes from feature branch”);
+System.out.println(“hello world changes from feature branch 2.0”);
+hello anu..am from main
+git reset test
 }
 }
